4d930d091cd62b249d032ac95713903a2c145ffa
Laura Arditti <laura.arditti@gmail.com>
2021-01-04 15:23:33 +0100

    Add evaluateObjective and estimatePotentialChange methods in ObjectiveFunction

--- it/polito/arditti/ObjectiveFunction.java
+++ it/polito/arditti/ObjectiveFunction.java
@@ -11,5 +11,20 @@ public class ObjectiveFunction {
     public ObjectiveFunction(TransitionData data) {
         this.transitionData = data;
     }
-    
+    public Double evaluateObjective(SimpleGraph<Integer, Integer[]> currentGraph) {
+        Double loss = 0.0;
+        for(Transition transition : transitionData.getPath()){
+            loss += Math.pow((transition.getPotentialChange()-estimatePotentialChange(transition,currentGraph)),2);
+        }
+        return loss/transitionData.size();
+    }
+    private double estimatePotentialChange(Transition transition, SimpleGraph<Integer, Integer[]> currentGraph) {
+        double estimate = 0.0;
+        Set<Transition> compatibleTransitions = transitionData.getCompatibleTransitions(transition,currentGraph);
+        for (Transition compatibleTransition : compatibleTransitions){
+            estimate+= compatibleTransition.getPotentialChange();
+        }
+        estimate = estimate / compatibleTransitions.size();
+        return estimate;
+    }
 }
