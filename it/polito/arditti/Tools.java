6ce676dcd520b17dfb8b12eb7bd9dedad7bf39d9
Laura Arditti <laura.arditti@gmail.com>
2021-01-04 19:31:58 +0100

    Add powerSet method in Tools

--- it/polito/arditti/Tools.java
+++ it/polito/arditti/Tools.java
@@ -1,8 +1,6 @@
 package it.polito.arditti;
 
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Tools {
@@ -24,5 +22,24 @@ public class Tools {
         Random rand = new Random();
         int index = rand.nextInt(elements.size());
         return elements.get(index);
+    }
+
+    public static <T> Set<Set<T>> powerSet(Set<T> originalSet) {
+        Set<Set<T>> sets = new HashSet<Set<T>>();
+        if (originalSet.isEmpty()) {
+            sets.add(new HashSet<T>());
+            return sets;
+        }
+        List<T> list = new ArrayList<T>(originalSet);
+        T head = list.get(0);
+        Set<T> rest = new HashSet<T>(list.subList(1, list.size()));
+        for (Set<T> set : powerSet(rest)) {
+            Set<T> newSet = new HashSet<T>();
+            newSet.add(head);
+            newSet.addAll(set);
+            sets.add(newSet);
+            sets.add(set);
+        }
+        return sets;
     }
 }
